ef0986fde086287c206ce703f1926d49d1038950
TheDeveloperXYZ <contact@thedeveloper.xyz>
2015-12-26 21:05:20 +0200

    Update

--- test/ttactionmirror.spec.js
+++ test/ttactionmirror.spec.js
@@ -1,8 +1,12 @@
 import expect from 'unexpected';
-import TTActionMirror from '../src/ttactionmirror.js';
+import actionMirror from '../src/ttactionmirror.js';
 
 describe('TTActionMirror', () => {
   it('should be defined', () => {
-    expect(TTActionMirror, 'to be defined');
+    expect(actionMirror, 'to be defined');
+  });
+
+  it('should return correct value', () => {
+    expect(actionMirror(), 'to be true');
   });
 });
